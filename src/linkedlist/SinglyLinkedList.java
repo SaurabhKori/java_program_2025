82fd18340830d7602267870327de625f66eafde2
SaurabhKori <27022002saurabh@gmail.com>
2025-12-31 00:14:01 +0530

    SinglyLinkedList

---
+++ src/linkedlist/SinglyLinkedList.java
@@ -0,0 +1,15 @@
+package linkedlist;
+
+public class SinglyLinkedList {
+	private ListNode head;
+
+	private static class ListNode {
+		private int data;
+		private ListNode next;
+
+		public ListNode(int data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
+}
