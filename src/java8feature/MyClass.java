1b7b8a8f619049d130c293b0462f763067e1a4ae
SaurabhKori <27022002saurabh@gmail.com>
2026-02-13 20:09:35 +0530

    "Functional Inter face "

--- src/java8feature/MyClass.java
+++ src/java8feature/MyClass.java
@@ -10,10 +10,16 @@ interface B {
 	default void sayHello() {
 		System.out.println("I  am B Boss");
 	}
+
+	static void sayOk() {
+		System.out.println("I  am B Boss Ok");
+	}
 }
 
 class C {
 	int a = 12;
+	static int b = 10;
+
 }
 
 public class MyClass extends C implements A, B {
@@ -27,9 +33,12 @@ public class MyClass extends C implements A, B {
 	@Override
 	public void sayHello() {
 		// TODO Auto-generated method stub
+		MyClass data = new MyClass();
 		A.super.sayHello();
-		System.out.println(super.a + this.a);
+		System.out.println(super.a + this.a + data.b);
 		B.super.sayHello();
+		B.sayOk();
 	}
+
 
 }
